cb25c88e144832314b87d6c241b6a05e5a85bfcc
Rodrigo Maturano Zanetti <rzanetti.dev@outlook.com>
2026-06-18 14:58:28 +0200

    Configures the centralized error middleware

--- app.js
+++ app.js
@@ -5,12 +5,34 @@ const auth = require('./routes/auth');
 const users = require('./routes/users');
 const mongoose = require('mongoose');
 const { PORT = 3000 } = process.env;
+const winston = require('winston');
+const expressWinston = require('express-winston');
 
 app.use(express.json());
+
+app.use(
+  expressWinston.logger({
+    transports: [new winston.transports.File({ filename: 'request.log' })],
+    format: winston.format.json(),
+  }),
+);
 
 app.use('/users', users);
 app.use('/articles', articles);
 app.use('/', auth);
+
+app.use(
+  expressWinston.errorLogger({
+    transports: [new winston.transports.File({ filename: 'error.log' })],
+    format: winston.format.json(),
+  }),
+);
+
+function errorMiddleware(err, req, res, next) {
+  res.status(500).send({ message: 'Ocorreu um erro' });
+}
+
+app.use(errorMiddleware);
 
 mongoose.connect('mongodb://localhost:27017/newsexplorer');
 
